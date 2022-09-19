b26c6a93810b012203b6502081278c1be14f5047
erikcaul <erikcaul@gmail.com>
2022-09-19 13:22:05 -0400

    add fetchApi and fetchCategory + session mentorat

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-class Film{
+/* class Film{
     constructor(jsonFilm){
         jsonFilm && Object.assign(this, jsonFilm);
     }   
-}
+} */
 
 /* 
 chaque film doit être un bloc html = un article
@@ -13,48 +13,35 @@ chaque film doit être un bloc html = un article
  + function qui prend en paramètre la categorie et le nombre de films à récupérer  (après avoir appelé la première)
 (un premier fetch pour un premier appel sur une première page et un second pour les 2 restant, il nous en faut 7)
  + function pour le meilleur film (sortby nimb + prendre le premier) (après avoir appelé la première)
-*/
+
+ */
 
 // Fetch API
-function fetchApi(){
-    return fetch("http://localhost:8000/api/v1/titles/")
-    .then(reponse => {
-        if (reponse.ok) {
-            return reponse.json();
-         } else {
-            throw new error("problem");
-            }
-    })
+async function fetchApi(path, query){
+    let response = await fetch(`http://localhost:8000/${path}?${query}`);
+    return await response.json();
 }
 
 let pageNb = "1";
 let filmCounter = 0;
 
 // Fetch with 2 parameters : category + number of films to fetch
-function fetchCategory(category, nbFilms, page){
-    // let urlApi =fetchApi();
-    fetch("http://localhost:8000/api/v1/titles/" + "?genre=" + category + "?page=" + page)
-    .then(reponse => {
-        if (reponse.ok) {
-            return reponse.json();
-         } else {
-            throw new error("problem");
-            }
-    })
+async function fetchCategory(category, nbFilms){
+    let films = [];
+    for(let i=1;films.length < nbFilms; i++) {
+        films.push(...await fetchApi("api/v1/titles/", `genre=${category}&page=${i}`));
+    }
+    // enlever les 3 derniers films
+    return films;    
 }
-    
+   
 // Fetch Best film
-function fetchBestFilm(){
-    fetch("http://localhost:8000/api/v1/titles/" + "?sort_by=imdb_score")
-    .then(reponse => {
-        if (reponse.ok) {
-            return reponse.json();
-         } else {
-            throw new error("problem");
-            }
-    })
+async function fetchBestFilm(){
+    let bestListFilm = await fetchApi("api/v1/titles/", `sort_by=imdb_score`); 
+    return bestListFilm[0];
 }
-    
+
+/* 
 fetchBestFilm().then(film => {
     for (film of film.results) {
         title = film.title;
@@ -80,7 +67,7 @@ fetchBestFilm().then(film => {
         testActors.innerHTML = `Acteurs : ${actors}`;
     }  
 })
-
+ */
 fetchCategory("Action/", 7, "1").then(film => {
     for (film of film.results) {
         title = film.title;
@@ -110,12 +97,9 @@ fetchCategory("Action/", 7, "1").then(film => {
             break;
         }
     }  
-})
+}) 
 
-
-
-
-/* function fetchFilm(genre, page){
+function fetchFilm(genre, page){
     return fetch("http://localhost:8000/api/v1/" + genre + page)
             .then(reponse => {
                 if (reponse.ok) {
@@ -124,33 +108,33 @@ fetchCategory("Action/", 7, "1").then(film => {
                     throw new error("problem");
                     }
                 }
-            )} */
-/* 
-for (let i=1;i<5;i++) {
+            )}
+
+for (let i=1;i<2;i++) {
     fetchFilm("titles/", "?page=" + i).then(film => {
         for (film of film.results) {
-            title = film.title;
+            filmTitle = film.title;
             image_url = film.image_url;
-            directors = film.directors;
+            /* directors = film.directors;
             actors = film.actors;
             writers = film.writers;
-            genres = film.genres;
-            let testTitle = document.createElement("testTitle");
-            let testImage = document.createElement("image");
-            let testGenre = document.createElement("p");
+            genres = film.genres; */
+            let title = document.createElement("title");
+            let image = document.createElement("image");
+            /* let testGenre = document.createElement("p");
             let testDirection = document.createElement("p");
-            let testActors = document.createElement("p");
-            document.getElementById("container").appendChild(testTitle);
-            document.getElementById("container").appendChild(testImage);
-            document.getElementById("container").appendChild(testGenre);
+            let testActors = document.createElement("p"); */
+            document.getElementById("container").appendChild(title);
+            document.getElementById("container").appendChild(image);
+            /* document.getElementById("container").appendChild(testGenre);
             document.getElementById("container").appendChild(testDirection);
-            document.getElementById("container").appendChild(testActors);
-            testTitle.innerHTML = "<strong>Titre :</strong> " + title;
-            testImage.innerHTML = `<img src= ${image_url}>`;
-            testGenre.innerHTML = `genres : ${genres}`;
+            document.getElementById("container").appendChild(testActors);*/
+            image.innerHTML = `<img src= ${image_url}>`;
+            title.innerHTML = "<strong>Titre :</strong> " + filmTitle;
+            
+            /* testGenre.innerHTML = `genres : ${genres}`;
             testDirection.innerHTML = `Direction : ${directors}`;
-            testActors.innerHTML = `Acteurs : ${actors}`;
+            testActors.innerHTML = `Acteurs : ${actors}`; */
         }
     })  
 }
- */
