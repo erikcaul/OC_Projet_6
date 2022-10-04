c8b9c1e11bc2a934549fa709fd5c12335bca7534
erikcaul <erikcaul@gmail.com>
2022-10-04 10:33:25 -0400

    add bestMovie Diplay function

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-/* class Film{
-    constructor(jsonFilm){
-        jsonFilm && Object.assign(this, jsonFilm);
-    }   
-} */
-
-/* 
-chaque film doit être un bloc html = un article
- -- section = category
- -- article = film
- 
- + faire une function fetch simple, appel API simplement
- + function qui prend en paramètre la categorie et le nombre de films à récupérer  (après avoir appelé la première)
-(un premier fetch pour un premier appel sur une première page et un second pour les 2 restant, il nous en faut 7)
- + function pour le meilleur film (sortby nimb + prendre le premier) (après avoir appelé la première)
-
- */
-
 // Fetch API
 async function fetchApi(path, query){
     let response = await fetch(`http://localhost:8000/${path}?${query}`);
     return await response.json();
 }
-
-let pageNb = "1";
-let filmCounter = 0;
 
 // Fetch with 2 parameters : category + number of films to fetch
 async function fetchCategory(category, nbMovies){
@@ -38,11 +17,20 @@ async function fetchCategory(category, nbMovies){
    
 // Fetch Best film
 async function fetchBestMovie(){
-    let bestListFilm = await fetchApi("api/v1/titles/", `sort_by=imdb_score`); 
-    return bestListFilm[0];
+    let bestListFilmResult = await fetchApi("api/v1/titles/", `sort_by=imdb_score`); 
+    let bestListMovies = bestListFilmResult.results;
+    console.log(bestListMovies);
+    let bestMovie = bestListMovies[0];
+    for (let j=0;j<5;j++){
+        bestMovie = bestListMovies[j];
+        if (bestMovie.image_url !== null){
+            return bestMovie;
+        }
+    }
 }
-
-async function DisplayMovies(category){
+ 
+// Display movies by category
+async function displayMovies(category){
     let movies = await fetchCategory(category,7);
     for (let i=0; i<movies.length;i++){
         let image_url = movies[i].image_url;
@@ -50,116 +38,18 @@ async function DisplayMovies(category){
         document.getElementById(category).appendChild(image);
         image.innerHTML = `<img src= ${image_url}>`;
     } 
-
-    console.log(movies);
     return movies;
 }
 
-DisplayMovies('action');
-
-/*
-let movies = fetchCategory("action", 7);
-for (let i=0; i<movies.length;i++){
-    let image_url = movies[i].image_url;
-    let image = document.createElement("imageMovie");
-    document.getElementsById(category).appendChild(image);
-    image.innerHTML = `<img src= ${image_url}>`;
-}  */
-/* 
-fetchBestFilm().then(film => {
-    for (film of film.results) {
-        title = film.title;
-        image_url = film.image_url;
-        directors = film.directors;
-        actors = film.actors;
-        writers = film.writers;
-        genres = film.genres;
-        let testTitle = document.createElement("testTitle");
-        let testImage = document.createElement("image");
-        let testGenre = document.createElement("p");
-        let testDirection = document.createElement("p");
-        let testActors = document.createElement("p");
-        document.getElementById("bestFilm").appendChild(testTitle);
-        document.getElementById("bestFilm").appendChild(testImage);
-        document.getElementById("bestFilm").appendChild(testGenre);
-        document.getElementById("bestFilm").appendChild(testDirection);
-        document.getElementById("bestFilm").appendChild(testActors);
-        testTitle.innerHTML = "<strong>Titre :</strong> " + title;
-        testImage.innerHTML = `<img src= ${image_url}>`;
-        testGenre.innerHTML = `genres : ${genres}`;
-        testDirection.innerHTML = `Direction : ${directors}`;
-        testActors.innerHTML = `Acteurs : ${actors}`;
-    }  
-})
- */
-/* fetchCategory("Action/", 7, "1").then(film => {
-    for (film of film.results) {
-        title = film.title;
-        image_url = film.image_url;
-        directors = film.directors;
-        actors = film.actors;
-        writers = film.writers;
-        genres = film.genres;
-        let testTitle = document.createElement("testTitle");
-        let testImage = document.createElement("image");
-        let testGenre = document.createElement("p");
-        let testDirection = document.createElement("p");
-        let testActors = document.createElement("p");
-        document.getElementById("container").appendChild(testTitle);
-        document.getElementById("container").appendChild(testImage);
-        document.getElementById("container").appendChild(testGenre);
-        document.getElementById("container").appendChild(testDirection);
-        document.getElementById("container").appendChild(testActors);
-        testTitle.innerHTML = "<strong>Titre :</strong> " + title;
-        testImage.innerHTML = `<img src= ${image_url}>`;
-        testGenre.innerHTML = `genres : ${genres}`;
-        testDirection.innerHTML = `Direction : ${directors}`;
-        testActors.innerHTML = `Acteurs : ${actors}`;
-    
-        filmCounter++;
-        if (filmCounter <= nbFilms){
-            break;
-        }
-    }  
-})  */
-/* 
-function fetchFilm(genre, page){
-    return fetch("http://localhost:8000/api/v1/" + genre + page)
-            .then(reponse => {
-                if (reponse.ok) {
-                    return reponse.json();
-                 } else {
-                    throw new error("problem");
-                    }
-                }
-            )}
-
-for (let i=1;i<2;i++) {
-    fetchFilm("titles/", "?page=" + i).then(film => {
-        for (film of film.results) {
-            filmTitle = film.title;
-            image_url = film.image_url;
-            /* directors = film.directors;
-            actors = film.actors;
-            writers = film.writers;
-            genres = film.genres;
-            let title = document.createElement("title");
-            let image = document.createElement("image");
-            let testGenre = document.createElement("p");
-            let testDirection = document.createElement("p");
-            let testActors = document.createElement("p");
-            document.getElementById("container").appendChild(title);
-            document.getElementById("container").appendChild(image);
-            document.getElementById("container").appendChild(testGenre);
-            document.getElementById("container").appendChild(testDirection);
-            document.getElementById("container").appendChild(testActors);
-            image.innerHTML = `<img src= ${image_url}>`;
-            title.innerHTML = "<strong>Titre :</strong> " + filmTitle;
-            
-            testGenre.innerHTML = `genres : ${genres}`;
-            testDirection.innerHTML = `Direction : ${directors}`;
-            testActors.innerHTML = `Acteurs : ${actors}`;
-        }
-    })  
+// Display best movie
+async function displayBestMovie(){
+    let bestMovie = await fetchBestMovie();
+    let imageBestMovie = bestMovie.image_url;
+    let imageBest = document.createElement("div");
+    document.getElementById("bestMovie").appendChild(imageBest);
+    imageBest.innerHTML = `<img src= ${imageBestMovie}>`;
+    return bestMovie;
 }
- */
+
+displayBestMovie();
+displayMovies('action');
